a52eeb172abbdbeaa4d4cd0fe4ec83f72b24519c
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-06-30 10:08:50 +0000

    improves the quality of charts

--- modules/visualization_tweets.py
+++ modules/visualization_tweets.py
@@ -15,7 +15,7 @@ class TweetDfVisualization:
         plt.imshow(WordCloud(width=1000,height=600,stopwords=STOPWORDS).generate(' '.join(self.df.cleaned_tweet.values)))
         plt.axis('off')
         plt.title('Most Frequent Words In Tweets',fontsize=16)
-        plt.savefig('wordcloud.png')
+        plt.savefig('../img/wordcloud.png')
         plt.show()
 
     def create_viz(self):
@@ -24,29 +24,50 @@ class TweetDfVisualization:
         yellow = '#FFC55C'
         blue = '#7EC8E3'
         green = '#00D100'
-
+        plt.rcParams['axes.labelsize'] = 20
+        #plt.rcParams['axes.titlesize'] = 16
+        
         category_color_dict = {'very negative':red, 'negative':yellow, 'neutral':blue, 'positive':green, 'very positive':dark_green}
         fig, axes = plt.subplots(2,2, figsize=(20, 20))
+        plt.rcParams.update({'axes.titlesize': 'Large'})
+        
+        axes[0,0].tick_params(axis='x', labelsize=16)
+        axes[0,0].tick_params(axis='y', labelsize=16)
+        
+        axes[0,1].tick_params(axis='x', labelsize=16)
+        axes[0,1].tick_params(axis='y', labelsize=16)
+        axes[0,1].set_title('POLARITY COUNT', fontsize=22)
+        
+        axes[1,0].tick_params(axis='x', labelsize=16)
+        axes[1,0].tick_params(axis='y', labelsize=16)
+        axes[1,0].set_title('POLARITY WITH SUBJECTIVITY', fontsize=22)
+        
+        axes[1,1].tick_params(axis='x', labelsize=16)
+        axes[1,1].tick_params(axis='y', labelsize=16)
+        axes[1,1].set_title('SUBJECTIVITY COUNT', fontsize=22)
+        
         temp_select_df = self.df['classified_polarity'].value_counts(sort=False)
         explode = (0.4, 0.4, 0.4, 0.4, 0.4)
         colors= [category_color_dict[i] for i in temp_select_df.index]
 
         temp_select_df.plot.pie(ax=axes[0,0] ,ylabel='', autopct='%1.1f%%',
-                 shadow=True, startangle=90, explode = explode, colors=colors)
+                 shadow=True, startangle=90, explode = explode, colors=colors, fontsize=16)
+        
 
         sns.countplot(ax=axes[1,0], data=self.df, x='classified_polarity', hue='classified_subjectivity', palette="ch:1")
         sns.set_context('paper')
         sns.set_style('dark')
-        axes[1,0].set(xlabel='Polarity', ylabel='Count', title='POLARITY WITH SUBJECTIVITY')
-        axes[1,0].legend(title='Subjectivity', loc='upper right')
-        plt.subplots_adjust(top = 0.99, bottom=0.1, hspace=0.2, wspace=0.4)
+        axes[1,0].set(xlabel='Polarity', ylabel='Count')
+        axes[1,0].legend(title='Subjectivity', loc='best', fontsize=14, title_fontsize=16)
+        plt.subplots_adjust(top = 0.96, bottom=0.05, hspace=0.3, wspace=0.4)
+        
 
         colors= [category_color_dict[i] for i in temp_select_df.index]
         temp_select_df = self.df['classified_polarity'].value_counts(sort=False)
-        temp_select_df.plot.bar(ax=axes[0,1] ,xlabel='Polarity', ylabel='Count', color=colors, title='POLARITY COUNT')
+        temp_select_df.plot.bar(ax=axes[0,1] ,xlabel='Polarity', ylabel='Count', color=colors)
 
         sns.countplot(ax=axes[1,1], data=self.df, x='classified_subjectivity', palette="ch:1")
-        axes[1,1].set(xlabel='Count', ylabel='Polarity', title='SUBJECTIVITY COUNT')
-        #plt.show()
+        axes[1,1].set(xlabel='Count', ylabel='Polarity')
 
-        plt.savefig('viz.png')
+        #plt.show()
+        plt.savefig('../img/viz.png')
